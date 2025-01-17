d541bde13152d86f01fca69b12095d6c5a61974c
Nitzan Naveh <soapierfriend@gmail.com>
2025-01-17 14:07:25 +0200

    Initial commit

---
+++ app/src/main/java/com/tic_tac_toe/student_management/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.tic_tac_toe.student_management
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    // TODO: Step 1 - "Add student" button
+    // TODO: Step 2 - Navigate to AddStudentActivity
+    // TODO - Step 3 - Create AddStudentLayout
+    // TODO - Step 4 - "Save student" button
+}
