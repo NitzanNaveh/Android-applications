82051eac495499a8f0235951ec0f84627990a86e
Nitzan Naveh <soapierfriend@gmail.com>
2025-01-18 20:49:43 +0200

    add student activity-added

--- app/src/main/java/com/tic_tac_toe/student_management/AddStudentActivity.kt
+++ app/src/main/java/com/tic_tac_toe/student_management/AddStudentActivity.kt
@@ -1,34 +1,20 @@
 package com.tic_tac_toe.student_management
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class AddStudentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_add_student)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        // TODO: Step 1 - "Add student" button
-        // TODO: Step 2 - Navigate to AddStudentActivity
-        // TODO - Step 3 - Create AddStudentLayout
-        // TODO - Step 4 - "Save student" button
-
-
-        val addStudentButton: Button = findViewById(R.id.main_activity_addStudent_btn)
-        addStudentButton.setOnClickListener {
-            val intent = Intent(this, AddStudentActivity::class.java)
-            startActivity(intent)
         }
     }
 }
